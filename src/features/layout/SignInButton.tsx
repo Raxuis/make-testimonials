1df6698429f1c7cead7ac6e208be0ad3f91e8e29
Raxuis <raclot.raphael@gmail.com>
2024-03-16 15:51:55 +0100

    Feat(sign in) : Implemented the sign in with google

--- src/features/layout/SignInButton.tsx
+++ src/features/layout/SignInButton.tsx
@@ -1,9 +1,11 @@
-import { signIn } from "next-auth/react"
+import { signIn } from "@/auth/auth"
 import { Button } from "@/components/ui/button"
 
 export const SignInButton = () => {
-  return <form><Button formAction={async () => {
-    "use server";
-    await signIn()
-  }} >Sign In</Button></form>
+  return <form>
+    <Button formAction={async () => {
+      "use server";
+      await signIn()
+    }} >Sign In</Button>
+  </form>
 }
