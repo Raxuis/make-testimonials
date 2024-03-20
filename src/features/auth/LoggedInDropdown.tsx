00aff8b1373d2935d6c96ec378d9aa55bec8dc77
Raxuis <raclot.raphael@gmail.com>
2024-03-20 11:12:52 +0100

    Feat(log in and log out) : Implemented the log in and log out with gmail account

---
+++ src/features/auth/LoggedInDropdown.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { PropsWithChildren } from "react"
+import { signOutAction } from "./auth.action";
+import { LogOut } from "lucide-react";
+
+export type LoggedInDropdownProps = PropsWithChildren;
+
+export const LoggedInDropdown = (props: LoggedInDropdownProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        {props.children}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <form>
+          <DropdownMenuItem onClick={() => {
+            signOutAction()
+          }}>
+            <LogOut size={16} className="mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </form>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -1,10 +1,10 @@
-import { SignInButton } from "./SignInButton"
+import { LoggedInButton } from "../auth/LoggedInButton"
 
 export const Header = async () => {
   return (
     <div className="flex items-center gap-4">
-      <h1 className="font-bold text-lg">Get Testimonials</h1>
-      <SignInButton />
+      <h1 className="font-bold text-lg flex-1">Get Testimonials</h1>
+      <LoggedInButton />
     </div>
   )
 }
