d56c23c8505532d19e7058c41d5860edae3b355d
Raxuis <raclot.raphael@gmail.com>
2024-04-01 16:19:14 +0200

    feat: :art: Add star when premium account

--- src/features/auth/LoggedInButton.tsx
+++ src/features/auth/LoggedInButton.tsx
@@ -1,21 +1,23 @@
-import { baseAuth } from "@/auth/auth"
 import { SignInButton } from "./SignInButton"
 import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { LoggedInDropdown } from "./LoggedInDropdown"
+import { currentUser } from "@/auth/current-user"
+import { Star } from "lucide-react"
 export const LoggedInButton = async () => {
-  const session = await baseAuth()
-  if (!session?.user) {
+  const user = await currentUser()
+  if (!user) {
     return <SignInButton />;
   }
   return (
     <LoggedInDropdown>
       <Button variant="outline" size="sm">
+        {user.plan === "PREMIUM" ? <Star size={14} className="mr-2" /> : null}
         <Avatar className="size-6">
-          <AvatarFallback>{session.user.name?.[0]}</AvatarFallback>
+          <AvatarFallback>{user.name?.[0]}</AvatarFallback>
           {
-            session.user.image ? (
-              <AvatarImage src={session.user.image} alt={`${session.user.name ?? "-"}'s profile picture`} />
+            user.image ? (
+              <AvatarImage src={user.image} alt={`${user.name ?? "-"}'s profile picture`} />
             ) : null
           }
         </Avatar>
