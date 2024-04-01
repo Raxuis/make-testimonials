06fcd773b30f932c819e0f993237da9170819531
Raxuis <raclot.raphael@gmail.com>
2024-04-01 15:44:33 +0200

    test(stripe): :white_check_mark: Fix bugs with payment

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export const POST = async (req: NextRequest) => {
-  const body = await req.json();
+  const body = await req.text();
 
   const stripeSignature = req.headers.get('stripe-signature');
 
@@ -27,31 +27,26 @@ export const POST = async (req: NextRequest) => {
   switch (event.type) {
     case "checkout.session.completed": {
       const session = event.data.object as Stripe.Checkout.Session;
-      const priceId = session.line_items?.data[0].price?.id;
       const customerId = session.customer as string;
 
-      if (!priceId) {
-        return NextResponse.json({ error: "No product id" }, { status: 400 });
+      const user = await prisma.user.findFirst({
+        where: {
+          stripeCustomerId: customerId,
+        },
+      })
+      if (!user) {
+        return NextResponse.json({ error: "User not found" }, { status: 400 });
       }
-      if (priceId === "price_1P0h0G01P4NEqDx0vY2MhhjV" || priceId === "prod_price_id") {
-        const user = await prisma.user.findFirst({
-          where: {
-            stripeCustomerId: customerId,
-          },
-        })
-        if (!user) {
-          return NextResponse.json({ error: "User not found" }, { status: 400 });
-        }
 
-        await prisma.user.update({
-          where: {
-            id: user.id,
-          },
-          data: {
-            plan: "PREMIUM",
-          },
-        })
-      }
+      await prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          plan: "PREMIUM",
+        },
+      })
+
       break;
     }
     default: {
