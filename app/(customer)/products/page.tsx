89abfad31ea747fc60f18ff805e8d219f0390ba5
Raxuis <raclot.raphael@gmail.com>
2024-04-06 12:50:27 +0200

    style(products): :art: Removed card component

--- app/(customer)/products/page.tsx
+++ app/(customer)/products/page.tsx
@@ -1,8 +1,14 @@
 import { requiredCurrentUser } from "@/auth/current-user";
-import { Layout, LayoutTitle } from "@/components/layout";
+import { Layout, LayoutDescription, LayoutTitle } from "@/components/layout";
 import { buttonVariants } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { prisma } from "@/prisma";
 import type { PageParams } from "@/types/next";
 import Link from "next/link";
@@ -12,7 +18,7 @@ export default async function RoutePage(props: PageParams<{}>) {
 
   const products = await prisma.product.findMany({
     where: {
-      userId: user.id
+      userId: user.id,
     },
     select: {
       id: true,
@@ -20,44 +26,62 @@ export default async function RoutePage(props: PageParams<{}>) {
       slug: true,
       _count: {
         select: {
-          reviews: true,
-        }
-      }
-    }
-  })
+          reviews: {
+            where: {
+              text: {
+                not: null,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
   return (
     <Layout>
       <div className="flex justify-between">
-        <LayoutTitle>Products</LayoutTitle>
-        <Link href="/products/new" className={buttonVariants({ size: "sm", variant: "secondary" })}>
-          Create product
+        <div className="space-y-0.5">
+          <LayoutTitle>Products</LayoutTitle>
+          <LayoutDescription>Create products to review.</LayoutDescription>
+        </div>
+
+        <Link
+          href={`/products/new`}
+          className={buttonVariants({ size: "sm", variant: "secondary" })}
+        >
+          Create
         </Link>
       </div>
-      <Card className="p-4">
-        {products.length ? (
-          <Table>
-            <TableHeader>
-              <TableHead>Name</TableHead>
-              <TableHead>Slug</TableHead>
-              <TableHead>Reviews</TableHead>
-            </TableHeader>
-            <TableBody>
-              {products.map((product) => (
-                <TableRow key={product.id}>
-                  <Link href={`/products/${product.id}`} key={product.id}>
-                    <TableCell>{product.name}</TableCell>
-                  </Link>
-                  <TableCell className="font-mono">{product.slug}</TableCell>
-                  <TableCell className="font-mono">{product._count.reviews}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>) : (
-          <Link href="/products/new" className="flex w-full items-center justify-center rounded-md border-2 border-dashed border-primary p-8 transition-colors hover:bg-accent/40 lg:p-12">
-            Create product
-          </Link>
-        )}
-      </Card>
+      {products.length ? (
+        <Table>
+          <TableHeader>
+            <TableHead>Name</TableHead>
+            <TableHead>Slug</TableHead>
+            <TableHead>Reviews</TableHead>
+          </TableHeader>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow key={product.id}>
+                <Link href={`/products/${product.id}`} key={product.id}>
+                  <TableCell>{product.name}</TableCell>
+                </Link>
+                <TableCell className="font-mono">{product.slug}</TableCell>
+                <TableCell className="font-mono">
+                  {product._count.reviews}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <Link
+          href="/products/new"
+          className="flex w-full items-center justify-center rounded-md border-2 border-dashed border-primary p-8 transition-colors hover:bg-accent/40 lg:p-12"
+        >
+          Create product
+        </Link>
+      )}
     </Layout>
-  )
+  );
 }
