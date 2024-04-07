e3786831c45d03413469031f45da23e12caa279c
Raxuis <raclot.raphael@gmail.com>
2024-04-07 13:22:35 +0200

    feat(pricing): :sparkles: Add upgrade plan page

---
+++ app/(customer)/upgrade/page.tsx
@@ -0,0 +1,21 @@
+import { requiredCurrentUser } from "@/auth/current-user";
+import { Layout, LayoutTitle } from "@/components/layout";
+import PricingPage from "@/features/landing/PricingSection";
+
+import type { PageParams } from "@/types/next";
+import { redirect } from "next/navigation";
+
+export default async function RoutePage(props: PageParams<{}>) {
+  const user = await requiredCurrentUser();
+
+  if (user.plan === "PREMIUM") {
+    redirect("/dashboard");
+  }
+
+  return (
+    <Layout>
+      <LayoutTitle>Upgrade</LayoutTitle>
+      <PricingPage />
+    </Layout>
+  );
+}
