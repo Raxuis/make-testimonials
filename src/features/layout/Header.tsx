83463e1fc56c095111b23b3d08e0992bdf5ce634
Raxuis <raclot.raphael@gmail.com>
2024-04-25 14:49:31 +0200

    fix(header): :art: Add padding on y axis

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 
 export const Header = async () => {
   return (
-    <header className="w-full border-b border-border">
+    <header className="w-full border-b border-border py-1">
       <Layout className="flex flex-row items-center gap-4">
         <Link href="/" className="flex-1">
           <Image src="/logo.svg" width={32} height={32} alt="maketestimonials logo" />
